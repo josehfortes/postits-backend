49bfac23ee52c6708fc0a4a1d8d24597ffbb0e2c
Jose Henrique <josehfortes@outlook.com>
2021-01-25 12:51:00 -0300

    CRUD para boards

--- src/modules/board/controller.js
+++ src/modules/board/controller.js
@@ -1,5 +1,9 @@
-import { created, serverError } from '../../utils/responses'
-import { createBoard } from './service'
+import {
+  badRequest, created, ok, serverError,
+} from '../../utils/responses'
+import {
+  createBoard, listBoards, getBoard, updateBoard, deleteBoard,
+} from './service'
 
 export const create = async (event) => {
   try {
@@ -8,6 +12,59 @@ export const create = async (event) => {
     const board = await createBoard(auth, body)
     return created(board)
   } catch (err) {
+    return serverError(err)
+  }
+}
+
+export const listAll = async (event) => {
+  try {
+    const auth = event.requestContext.authorizer
+    const boards = await listBoards(auth)
+    return ok(boards)
+  } catch (err) {
+    return serverError(err)
+  }
+}
+
+export const get = async (event) => {
+  try {
+    const { queryStringParameters } = event
+    if (!queryStringParameters || !queryStringParameters.id) throw new Error('id_required')
+
+    const auth = event.requestContext.authorizer
+    const board = await getBoard(auth, queryStringParameters.id)
+    return ok(board)
+  } catch (err) {
+    if (err.message === 'id_required') return badRequest(err.message)
+    return serverError(err)
+  }
+}
+
+export const put = async (event) => {
+  try {
+    const { queryStringParameters } = event
+    if (!queryStringParameters || !queryStringParameters.id) throw new Error('id_required')
+
+    const auth = event.requestContext.authorizer
+    const body = JSON.parse(event.body)
+    const updatedBoard = await updateBoard(auth, queryStringParameters.id, body)
+    return ok(updatedBoard)
+  } catch (err) {
+    if (err.message === 'id_required') return badRequest(err.message)
+    return serverError(err)
+  }
+}
+
+export const deleteController = async (event) => {
+  try {
+    const { queryStringParameters } = event
+    if (!queryStringParameters || !queryStringParameters.id) throw new Error('id_required')
+
+    const auth = event.requestContext.authorizer
+    await deleteBoard(auth, queryStringParameters.id)
+    return ok()
+  } catch (err) {
+    if (err.message === 'id_required') return badRequest(err.message)
     return serverError(err)
   }
 }
--- src/modules/board/service.js
+++ src/modules/board/service.js
@@ -10,3 +10,34 @@ export const createBoard = async (user, data) => {
     updatedDate: new Date(),
   })
 }
+
+export const listBoards = async (user) => {
+  await connectToDatabase()
+  return BoardSchema.find({ owner: user.id })
+    .limit(6)
+    .select('name updatedDate')
+    .lean()
+    .exec()
+}
+
+export const getBoard = async (user, id) => {
+  await connectToDatabase()
+  return BoardSchema.findOne({ owner: user.id, _id: id })
+    .lean()
+    .exec()
+}
+
+export const updateBoard = async (user, id, data) => {
+  await connectToDatabase()
+  return BoardSchema.findOneAndUpdate({ owner: user.id, _id: id }, {
+    $set: {
+      ...data,
+      updatedDate: new Date(),
+    },
+  }, { new: true }).lean().exec()
+}
+
+export const deleteBoard = async (user, id) => {
+  await connectToDatabase()
+  return BoardSchema.findOneAndDelete({ owner: user.id, _id: id })
+}
