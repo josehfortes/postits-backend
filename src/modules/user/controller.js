a5da279108af91c75e7283a5d258b32070963d96
Jose Henrique <josehfortes@outlook.com>
2021-01-25 09:33:42 -0300

    Criado controller de user

---
+++ src/modules/user/controller.js
@@ -0,0 +1,17 @@
+
+export const createAccount = async (event) => {
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Create account works',
+        input: event
+      })
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err)
+    }
+  }
+}
