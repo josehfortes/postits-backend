79222af45bd453ea2dbf9d86644c0411041aa435
Jose Henrique <josehfortes@outlook.com>
2021-01-24 23:12:40 -0300

    Configurado webpack e serverless

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const slsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  devtool: 'source-map',
+  externals: [nodeExternals()],
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    // nao vamos minimizar o código por enqnt
+    minimize: false
+  },
+  performance: {
+    //desabilita warnings sobre o tamanho das entry points
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
