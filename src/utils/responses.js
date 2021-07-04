fade077feec8bfd9e55c8b3b2a7f520a34e3e3ef
Jose Henrique <vasco0007@gmail.com>
2021-07-04 18:55:53 -0300

    corrigido cors

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -1,6 +1,12 @@
+const headers = {
+  'Access-Control-Allow-Origin': `${process.env.SITE_URL}`,
+  'Access-Control-Allow-Credentials': true,
+}
+
 export const ok = (data) => {
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify(data),
   }
 }
@@ -8,6 +14,7 @@ export const ok = (data) => {
 export const created = (data) => {
   return {
     statusCode: 201,
+    headers,
     body: JSON.stringify(data),
   }
 }
@@ -15,6 +22,7 @@ export const created = (data) => {
 export const badRequest = (data) => {
   return {
     statusCode: 400,
+    headers,
     body: JSON.stringify(data),
   }
 }
@@ -22,6 +30,7 @@ export const badRequest = (data) => {
 export const unauthorized = (data) => {
   return {
     statusCode: 401,
+    headers,
     body: JSON.stringify(data),
   }
 }
@@ -29,6 +38,7 @@ export const unauthorized = (data) => {
 export const serverError = (data) => {
   return {
     statusCode: 500,
+    headers,
     body: JSON.stringify(data),
   }
 }
